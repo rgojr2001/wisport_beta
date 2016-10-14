aec4a5119b75feb89af9c237cfe4c6857ac99745
RJ Ochmann <rgojr2001@yahoo.com>
2016-10-14 15:27:18 -0500

    datatables

---
+++ app/Http/Controllers/DatatablesController.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\App\Models\WisportRacer;
+use App\Http\Requests;
+use App\User;
+use Yajra\Datatables\Datatables;
+use Illuminate\Support\Facades\DB;
+
+class DatatablesController extends Controller
+{
+    /**
+     * Displays datatables front end view
+     *
+     * @return \Illuminate\View\View
+     */
+    public function getIndex()
+    {
+        return view('datatables.index');
+    }
+
+    /**
+     * Process datatables ajax request.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function anyData()
+    {
+        return Datatables::of(WisportRacer::query())->make(true);
+    }
+
+}
