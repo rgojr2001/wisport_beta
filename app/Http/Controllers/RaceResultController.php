167a0c2c193828885be2e74ebab66e9220eec878
RJ Ochmann <rgojr2001@yahoo.com>
2016-10-14 15:27:26 -0500

    datatables

---
+++ app/Http/Controllers/RaceResultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\App\Models\RaceResult;
+
+class RaceResultController extends Controller
+{
+    //
+}
