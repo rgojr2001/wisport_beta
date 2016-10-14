c9824e3d07a66de8ddb53031cce1d41d493c1ebe
RJ Ochmann <rgojr2001@yahoo.com>
2016-10-14 16:05:40 -0500

    datatables - sql fix

--- app/Http/Controllers/SeasonsController.php
+++ app/Http/Controllers/SeasonsController.php
@@ -30,7 +30,7 @@ class SeasonsController extends Controller
     
     public function anyData(){
         return Datatables::of(DB::table('processed_overall')
-            ->join('races','races.id','=','processed_overall.event_id')
+            ->join('races','races.id','=','processed_overall.race_id')
             ->select('short_name','place','first', 'last','gender','processed_overall.time','ag_label','ag_place'))->make(true);
     }
 }
